1d4e7725f9d01c75118b468de75284e335892bea
Afnan543 <afnan.abid54@gmail.com>
2025-02-01 19:47:25 +0500

    Commit

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://cart-management-frontend-two.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(bodyParser.json());
 
 // Add logging middleware
