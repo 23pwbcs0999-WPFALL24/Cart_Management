7692cf679ddfd16cc235c1e0033a90a1b726d7f4
Afnan543 <afnan.abid54@gmail.com>
2025-02-01 19:08:56 +0500

    Final commit

--- Frontend/Cart Manager/vite.config.js
+++ Frontend/Cart Manager/vite.config.js
@@ -4,16 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Ensures that assets are served correctly in a relative path for deployment
+  base: "./", // For relative paths
   build: {
     outDir: "dist", // Vercel serves from 'dist'
-    rollupOptions: {
-      output: {
-        manualChunks: undefined, // Helps with chunking issues, remove if you don't need manual chunking
-      },
-    },
   },
   server: {
-    historyApiFallback: true, // Ensures client-side routing works for SPA (important for React)
+    historyApiFallback: true, // Ensures routing works correctly for SPAs
   },
 });
