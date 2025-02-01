fbeb4403dae015a310d906625a07e9e734271717
Afnan543 <afnan.abid54@gmail.com>
2025-02-01 19:36:35 +0500

    commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
-import Navbar from "./components/Navbar.jsx";
+import Navbar from "./Components/Navbar.jsx";
 import Home from "./pages/Home";
 import CartPage from "./pages/CartPage";
 import "./index.css";
